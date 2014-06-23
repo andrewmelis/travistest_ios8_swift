2485c22db84c5e6154278c53a4b7e448354232dc
Andrew Melis <amelis@pathf.com>
2014-06-23 14:42:20 -0500

    initial commit

---
+++ TravisTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisTest
+//
+//  Created by Andrew Melis on 6/23/14.
+//  Copyright (c) 2014 Andrew Melis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
